0d0628ffec3055a5143e7962316c0109e408c94e
Aline Bevilacqua <Aliine98@users.noreply.github.com>
2023-03-28 19:11:09 -0300

    ✨ feat: função também muda cor da palavra e botão

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,13 @@
-let imagem = document.querySelector(".starbucks")
-let circulo = document.querySelector(".circulo")
-
 function trocaImagem(endereco) {
-   imagem.src = endereco
+	const imagem = document.querySelector(".starbucks");
+	imagem.src = endereco;
 }
 
 function trocaCor(cor) {
-   circulo.style.background = cor
+	const circulo = document.querySelector(".circulo");
+	const texto = document.querySelector(".caixa-texto span");
+	const botao = document.querySelector(".caixa-texto button");
+	circulo.style.background = cor;
+	texto.style.color = cor;
+	botao.style.background = cor;
 }
